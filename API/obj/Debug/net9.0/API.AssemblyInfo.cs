d01c97d29dd5a8ea6ae3c2936547551e9c524d04
Alperenhub <kilincfurkanalperen@gmail.com>
2025-05-16 18:51:27 +0300

    error messages

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+885e63bb6fd460ac7aa22fce83f8fb5fee4cb06d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc26b811253a8b193b038b1c6921afe51c42a934")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
