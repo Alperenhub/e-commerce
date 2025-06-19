af2e0c23d8832b2fc8897590dd7532cf346e5c7e
Alperenhub <kilincfurkanalperen@gmail.com>
2025-06-19 23:07:10 +0300

    .

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b436ea1d4b923eec1c8e3fb6141d004d4e1b03c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d10634f2345e998af7bff580604732dbc3268a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
