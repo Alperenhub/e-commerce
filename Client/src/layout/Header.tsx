389c77523629bc1ca907c3efed65ae5016272819
Alperenhub <kilincfurkanalperen@gmail.com>
2025-08-12 23:11:58 +0300

    header

--- Client/src/layout/Header.tsx
+++ Client/src/layout/Header.tsx
@@ -1,9 +1,10 @@
-import { ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, Box, Button, IconButton, List, ListItem, Stack, Toolbar, Typography } from "@mui/material";
+import { KeyboardArrowDown, ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, Button, Container, IconButton, List, ListItem, Menu, MenuItem, Stack, Toolbar } from "@mui/material";
 import { Link, NavLink } from "react-router";
 import { logout } from "../features/account/accountSlice";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { clearCart } from "../features/counter/cartSlice";
+import { useState } from "react";
 
 const links = [
   { title: "Home", to: "/"},
@@ -33,6 +34,18 @@ const navStyles = {
 
 // Header component
  export const Header = () => {
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  function handleMenuClick(event: React.MouseEvent<HTMLButtonElement>){
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose(){
+    setAnchorEl(null);
+  }
+
   const {cart} = useAppSelector(state=> state.cart);
   const {user} = useAppSelector(state=> state.account);
   const dispatch = useAppDispatch();
@@ -41,9 +54,9 @@ const navStyles = {
   const itemCount = cart?.cartItems?.reduce((total,item)=>total+ item.quantity,0);
     return(
      <AppBar position="static" sx={{mb:4}}>
-      <Toolbar sx={{display:"flex", justifyContent:"space-between"}}>
+      <Container maxWidth="lg">
+      <Toolbar disableGutters sx={{display:"flex", justifyContent:"space-between"}}>
         <Box sx={{display: "flex", alignItems:"center"}}>
-        <Typography variant="h6">E-commerce</Typography>
         <List sx={{display:"flex"}}>
           {links.map(link =>
             <ListItem key={link.to} component={NavLink} to={link.to} sx={navStyles}>{link.title}</ListItem>
@@ -61,15 +74,22 @@ const navStyles = {
             {
               user ? (
                 <>
-                <Stack direction="row">
-                  <Button sx={navStyles}>{user.name}</Button>
-                  <Button sx={navStyles} onClick={()=>
+
+                  <Button id="user-button" onClick={handleMenuClick}
+                  endIcon={<KeyboardArrowDown/>}
+                   sx={navStyles}>{user.name}</Button>
+
+                      <Menu id="user-menu" anchorEl={anchorEl} open={open} onClose={handleClose}>
+                      <MenuItem>Orders</MenuItem>
+                      <MenuItem  onClick={()=>
                     {
                     dispatch(logout())
                     dispatch(clearCart())
-                    }
-                    }>Log Out</Button>
-                </Stack>
+                    }}>Logout</MenuItem>
+                      
+                    </Menu>
+
+
                 </>
               ):(
                 <>
@@ -86,6 +106,8 @@ const navStyles = {
         </Box>
 
       </Toolbar>
+      </Container>
+
      </AppBar>
     )
   };
