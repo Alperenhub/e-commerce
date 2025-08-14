c56b6d8952fd0643489f7dd0e28d4b49be2ff301
Alperenhub <kilincfurkanalperen@gmail.com>
2025-08-14 23:29:09 +0300

    payment api

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -1,10 +1,10 @@
-
-
-
 using API.Data;
 using API.DTO;
 using API.Entity;
 using API.Extensions;
+using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,14 +14,16 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class OrdersController : ControllerBase
     {
-        public readonly DataContext _context;
+        private readonly DataContext _context;
+        private readonly IConfiguration _config;
 
-        public OrdersController(DataContext context)
+        public OrdersController(DataContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
-       [HttpGet]
+        [HttpGet]
         public async Task<ActionResult<List<OrderDTO>>> GetOrder()
         {
             return await _context.Orders
@@ -30,7 +32,7 @@ namespace API.Controllers
             .Where(i => i.CustomerId == User.Identity!.Name)
             .ToListAsync();
         }
-         [HttpGet("{id}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<OrderDTO?>> GetOrder(int id)
         {
             return await _context.Orders
@@ -51,12 +53,12 @@ namespace API.Controllers
 
             if (cart == null) return BadRequest(new ProblemDetails { Title = "Problem getting cart" });
 
-            var items = new List<OrderItem>();
+            var items = new List<Entity.OrderItem>();
 
             foreach (var item in cart.CartItems)
             {
                 var product = await _context.Products.FindAsync(item.ProductId);
-                var orderItem = new OrderItem
+                var orderItem = new Entity.OrderItem
                 {
                     ProductId = product!.Id,
                     ProductName = product.Name!,
@@ -83,7 +85,11 @@ namespace API.Controllers
                 SubTotal = subTotal,
                 DeliveryFee = deliveryFee
             };
-            _context.Add(order);
+
+
+            //payment
+            await ProcessPayment();
+            _context.Orders.Add(order);
             _context.Carts.Remove(cart);
 
             var result = await _context.SaveChangesAsync() > 0;
@@ -93,5 +99,99 @@ namespace API.Controllers
 
             return BadRequest(new ProblemDetails { Title = "Problem getting cart" });
         }
+
+
+        private async Task<Payment> ProcessPayment()
+        {
+            Options options = new Options();
+            options.ApiKey = _config["PaymentAPI:APIKey"];
+options.SecretKey = _config["PaymentAPI:SecretKey"];
+options.BaseUrl = "https://sandbox-api.iyzipay.com";
+		
+CreatePaymentRequest request = new CreatePaymentRequest();
+request.Locale = Locale.TR.ToString();
+request.ConversationId = "123456789";
+request.Price = "1";
+request.PaidPrice = "1.2";
+request.Currency = Currency.TRY.ToString();
+request.Installment = 1;
+request.BasketId = "B67832";
+request.PaymentChannel = PaymentChannel.WEB.ToString();
+request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
+
+PaymentCard paymentCard = new PaymentCard();
+paymentCard.CardHolderName = "John Doe";
+paymentCard.CardNumber = "5528790000000008";
+paymentCard.ExpireMonth = "12";
+paymentCard.ExpireYear = "2030";
+paymentCard.Cvc = "123";
+paymentCard.RegisterCard = 0;
+request.PaymentCard = paymentCard;
+
+Buyer buyer = new Buyer();
+buyer.Id = "BY789";
+buyer.Name = "John";
+buyer.Surname = "Doe";
+buyer.GsmNumber = "+905350000000";
+buyer.Email = "email@email.com";
+buyer.IdentityNumber = "74300864791";
+buyer.LastLoginDate = "2015-10-05 12:43:35";
+buyer.RegistrationDate = "2013-04-21 15:12:09";
+buyer.RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+buyer.Ip = "85.34.78.112";
+buyer.City = "Istanbul";
+buyer.Country = "Turkey";
+buyer.ZipCode = "34732";
+request.Buyer = buyer;
+
+Address shippingAddress = new Address();
+shippingAddress.ContactName = "Jane Doe";
+shippingAddress.City = "Istanbul";
+shippingAddress.Country = "Turkey";
+shippingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+shippingAddress.ZipCode = "34742";
+request.ShippingAddress = shippingAddress;
+
+Address billingAddress = new Address();
+billingAddress.ContactName = "Jane Doe";
+billingAddress.City = "Istanbul";
+billingAddress.Country = "Turkey";
+billingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+billingAddress.ZipCode = "34742";
+request.BillingAddress = billingAddress;
+
+List<BasketItem> basketItems = new List<BasketItem>();
+BasketItem firstBasketItem = new BasketItem();
+firstBasketItem.Id = "BI101";
+firstBasketItem.Name = "Binocular";
+firstBasketItem.Category1 = "Collectibles";
+firstBasketItem.Category2 = "Accessories";
+firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+firstBasketItem.Price = "0.3";
+basketItems.Add(firstBasketItem);
+
+BasketItem secondBasketItem = new BasketItem();
+secondBasketItem.Id = "BI102";
+secondBasketItem.Name = "Game code";
+secondBasketItem.Category1 = "Game";
+secondBasketItem.Category2 = "Online Game Items";
+secondBasketItem.ItemType = BasketItemType.VIRTUAL.ToString();
+secondBasketItem.Price = "0.5";
+basketItems.Add(secondBasketItem);
+
+BasketItem thirdBasketItem = new BasketItem();
+thirdBasketItem.Id = "BI103";
+thirdBasketItem.Name = "Usb";
+thirdBasketItem.Category1 = "Electronics";
+thirdBasketItem.Category2 = "Usb / Cable";
+thirdBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+thirdBasketItem.Price = "0.2";
+basketItems.Add(thirdBasketItem);
+request.BasketItems = basketItems;
+
+return await Payment.Create(request, options);
+        }
+
+
     }
 }
